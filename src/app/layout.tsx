66302be5c1497bfae8d28b286a7f38fc54e82e1b
KingDonggyu <dgkim69166916@gmail.com>
2024-01-11 18:48:39 +0900

    chore: 재사용 가능한 컴포넌트 모음 shadcn/ui 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={inter.className}>{children}</body>
     </html>
   );
