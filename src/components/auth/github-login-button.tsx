fff39fefa5079ddac8bcde929a040aba3b9a54fb
KingDonggyu <dgkim69166916@gmail.com>
2024-01-12 18:11:22 +0900

    feat: github oauth 로그인 버튼 내 링크 추가

--- src/components/auth/github-login-button.tsx
+++ src/components/auth/github-login-button.tsx
@@ -1,11 +1,16 @@
 import { Button } from '@/components/ui/button';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 export const GitHubLoginButton = () => {
+  const githubOAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID}`;
+
   return (
-    <Button className="h-auto" variant="outline">
-      <GitHubLogoIcon width={30} height={30} />
-      <span className="text-lg ml-2">GitHub로 시작하기</span>
+    <Button className="h-auto" variant="outline" asChild>
+      <Link href={githubOAuthUrl}>
+        <GitHubLogoIcon width={30} height={30} />
+        <span className="text-lg ml-2">GitHub로 시작하기</span>
+      </Link>
     </Button>
   );
 };
