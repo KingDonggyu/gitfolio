28ffd99c85d74ae445f9c83456e40b1d3f462750
KingDonggyu <dgkim69166916@gmail.com>
2024-01-12 16:35:46 +0900

    refactor: tailwind 'tw-' prefix 제거

--- src/components/auth/github-login-button.tsx
+++ src/components/auth/github-login-button.tsx
@@ -3,9 +3,9 @@ import { GitHubLogoIcon } from '@radix-ui/react-icons';
 
 export const GitHubLoginButton = () => {
   return (
-    <Button className="tw-h-auto" variant="outline">
+    <Button className="h-auto" variant="outline">
       <GitHubLogoIcon width={30} height={30} />
-      <span className="tw-text-lg tw-ml-2">GitHub로 시작하기</span>
+      <span className="text-lg ml-2">GitHub로 시작하기</span>
     </Button>
   );
 };
