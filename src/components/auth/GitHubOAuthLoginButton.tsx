bb2d93fbcbe5482e8ddefb56a19e669fef4af959
KingDonggyu <dgkim69166916@gmail.com>
2024-01-12 16:19:21 +0900

    feat: GitHub OAuth 로그인 버튼 컴포넌트 디자인 구현

---
+++ src/components/auth/GitHubOAuthLoginButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+
+export const GitHubOAuthLoginButton = () => {
+  return (
+    <Button className="tw-h-auto" variant="outline">
+      <GitHubLogoIcon width={30} height={30} />
+      <span className="tw-text-lg tw-ml-2">GitHub로 시작하기</span>
+    </Button>
+  );
+};
