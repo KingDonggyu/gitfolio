e1b30048637db2e32dd8260a7a0ef0be3efd5a43
KingDonggyu <dgkim69166916@gmail.com>
2024-01-14 15:05:48 +0900

    feat: Pretandard 로컬 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,28 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
+
 import { ThemeProvider } from '@/components/theme/theme-provider';
 import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +32,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body className={pretendard.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
